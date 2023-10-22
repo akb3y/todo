2808933160f02fc3000ef077b023cfaa4345a74c
Alisha Barraw <Alisha.Barraw@gmail.com>
2023-10-22 17:44:50 -0600

    added taskform

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import TaskForm from "./components/TaskForm.jsx";
 
 const App = () => {
-  return <h1>Hello world! I am using React</h1>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/tasks")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error(error));
+  }, [data]);
+
+  return (
+    <div>
+      <TaskForm />
+    </div>
+  );
 };
 
 export default App;
